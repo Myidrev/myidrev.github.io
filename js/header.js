06d9c34ed791df6bbe626e1a4584cf324049e283
Myidrev <wiskaverdian@gmail.com>
2023-06-12 13:50:03 +0700

    first commit

---
+++ js/header.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navigation');
+let navbar2 = document.querySelector ('.hambuger')
+
+
+menu.onclick = () => {
+    // menu.classList.toggle('bx-x')
+    navbar.classList.toggle('open');
+    navbar2.classList.toggle('open');
+}
+
+// sticky
+
